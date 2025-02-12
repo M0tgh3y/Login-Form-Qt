3a81cd21dc5b376ff01d146756b94eaf98712040
M0tgh3y <mot13832004@gmail.com>
2025-02-12 13:05:27 +0330

    Change in the appearance

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -11,12 +11,15 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -25,11 +28,17 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QWidget *gridLayoutWidget;
+    QGridLayout *gridLayout;
+    QVBoxLayout *verticalLayout;
     QLabel *nameofprogram;
+    QHBoxLayout *horizontalLayout_2;
     QLabel *username;
-    QLabel *password;
     QLineEdit *useredit;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *password;
     QLineEdit *passedit;
+    QHBoxLayout *horizontalLayout;
     QPushButton *okbutton;
     QPushButton *exit_button;
     QMenuBar *menubar;
@@ -39,45 +48,94 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(800, 600);
+        MainWindow->resize(573, 433);
+        MainWindow->setLayoutDirection(Qt::LayoutDirection::LeftToRight);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
-        nameofprogram = new QLabel(centralwidget);
+        gridLayoutWidget = new QWidget(centralwidget);
+        gridLayoutWidget->setObjectName("gridLayoutWidget");
+        gridLayoutWidget->setGeometry(QRect(20, 20, 531, 361));
+        gridLayout = new QGridLayout(gridLayoutWidget);
+        gridLayout->setObjectName("gridLayout");
+        gridLayout->setContentsMargins(0, 0, 0, 0);
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(20);
+        verticalLayout->setObjectName("verticalLayout");
+        verticalLayout->setContentsMargins(10, 10, 10, 10);
+        nameofprogram = new QLabel(gridLayoutWidget);
         nameofprogram->setObjectName("nameofprogram");
-        nameofprogram->setGeometry(QRect(90, 20, 411, 61));
         QFont font;
         font.setPointSize(20);
         nameofprogram->setFont(font);
-        username = new QLabel(centralwidget);
+
+        verticalLayout->addWidget(nameofprogram);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setSpacing(50);
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        horizontalLayout_2->setContentsMargins(10, 10, 10, 10);
+        username = new QLabel(gridLayoutWidget);
         username->setObjectName("username");
-        username->setGeometry(QRect(50, 120, 221, 61));
         QFont font1;
         font1.setPointSize(15);
         username->setFont(font1);
-        password = new QLabel(centralwidget);
-        password->setObjectName("password");
-        password->setGeometry(QRect(50, 190, 221, 61));
-        password->setFont(font1);
-        useredit = new QLineEdit(centralwidget);
+
+        horizontalLayout_2->addWidget(username);
+
+        useredit = new QLineEdit(gridLayoutWidget);
         useredit->setObjectName("useredit");
-        useredit->setGeometry(QRect(240, 140, 281, 28));
         useredit->setFont(font1);
-        passedit = new QLineEdit(centralwidget);
+
+        horizontalLayout_2->addWidget(useredit);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setSpacing(50);
+        horizontalLayout_3->setObjectName("horizontalLayout_3");
+        horizontalLayout_3->setContentsMargins(10, 10, 10, 10);
+        password = new QLabel(gridLayoutWidget);
+        password->setObjectName("password");
+        password->setFont(font1);
+
+        horizontalLayout_3->addWidget(password);
+
+        passedit = new QLineEdit(gridLayoutWidget);
         passedit->setObjectName("passedit");
-        passedit->setGeometry(QRect(240, 210, 281, 28));
         passedit->setFont(font1);
-        okbutton = new QPushButton(centralwidget);
+
+        horizontalLayout_3->addWidget(passedit);
+
+
+        verticalLayout->addLayout(horizontalLayout_3);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(60);
+        horizontalLayout->setObjectName("horizontalLayout");
+        horizontalLayout->setContentsMargins(70, -1, 70, -1);
+        okbutton = new QPushButton(gridLayoutWidget);
         okbutton->setObjectName("okbutton");
-        okbutton->setGeometry(QRect(150, 300, 83, 41));
         okbutton->setFont(font1);
-        exit_button = new QPushButton(centralwidget);
+
+        horizontalLayout->addWidget(okbutton);
+
+        exit_button = new QPushButton(gridLayoutWidget);
         exit_button->setObjectName("exit_button");
-        exit_button->setGeometry(QRect(280, 300, 83, 41));
         exit_button->setFont(font1);
+
+        horizontalLayout->addWidget(exit_button);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+
+        gridLayout->addLayout(verticalLayout, 0, 0, 1, 1);
+
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 25));
+        menubar->setGeometry(QRect(0, 0, 573, 25));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
@@ -93,7 +151,7 @@ public:
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
         nameofprogram->setText(QCoreApplication::translate("MainWindow", "Login Management System", nullptr));
         username->setText(QCoreApplication::translate("MainWindow", "User Name:", nullptr));
-        password->setText(QCoreApplication::translate("MainWindow", "Password:", nullptr));
+        password->setText(QCoreApplication::translate("MainWindow", "Password:   ", nullptr));
         okbutton->setText(QCoreApplication::translate("MainWindow", "OK", nullptr));
         exit_button->setText(QCoreApplication::translate("MainWindow", "Exit", nullptr));
     } // retranslateUi
